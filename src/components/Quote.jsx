ed63cf151f3349f5c7beb2306c26a72e32a9ca1d
leandro <leaguts21@gmail.com>
2022-04-13 18:04:52 -0500

    finalizado

---
+++ src/components/Quote.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Quote = ({quote}) => {
+  return (
+    <div><p>
+    {quote.text} <br/>
+    <span> {quote.author} </span>
+  </p></div>
+  )
+}
+
+export default Quote
